d0292bcdb606caec06f9d9f8a695c1ad0bb98ace
Hyunwoo <nhw3990@kau.kr>
2024-11-07 23:17:49 +0900

    refactor: AppRoutes에서 Router 중복 제거 및 라우팅 구조 개선

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 // src/routes/index.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import UserDashboard from "../pages/UserDashboard/UserDashboard";
 import AdminDashboard from "../pages/AdminDashboard/AdminDashboard";
@@ -8,14 +8,12 @@ import AuthCallback from "../pages/AuthCallback"; // 새로 추가
 
 function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/auth/kakao/callback" element={<AuthCallback />} /> {/* AuthCallback 라우트 추가 */}
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/user-dashboard" element={<UserDashboard />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      <Route path="/auth/kakao/callback" element={<AuthCallback />} />
+    </Routes>
   );
 }
 
