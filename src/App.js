32f2daa4dc9f7e524008ed3233148f0cb1b0fdd7
Hyunwoo <nhw3990@kau.kr>
2024-11-07 23:17:44 +0900

    fix: App 컴포넌트 최상위에 Router 추가하여 Link 오류 해결

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 // src/App.js
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom"; // Router 임포트
 import AppRoutes from "./routes";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
-      <main className="flex-grow p-4">
-        <AppRoutes /> {/* 페이지 내용이 여기에 표시됩니다 */}
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow p-4">
+          <AppRoutes />
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
