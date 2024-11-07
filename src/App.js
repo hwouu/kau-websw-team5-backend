1472dfe0804a2f4fa7ee23ca1250e5ee00dd2106
Hyunwoo <nhw3990@kau.kr>
2024-11-07 23:08:31 +0900

    refactor: App 컴포넌트에 AppRoutes 통합하여 라우팅 구조 개선

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import AppRoutes from "./routes";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow p-4">
+        <AppRoutes /> {/* 페이지 내용이 여기에 표시됩니다 */}
+      </main>
+      <Footer />
     </div>
   );
 }
