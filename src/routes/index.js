d2ba95263c8d7d3c2c504c4a86a6b62dad49c47e
Hyunwoo <nhw3990@kau.kr>
2024-11-08 14:07:18 +0900

    feat: 새로운 페이지 및 경로 추가 (신고 및 관리 페이지)

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,11 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import UserDashboard from "../pages/UserDashboard/UserDashboard";
 import AdminDashboard from "../pages/AdminDashboard/AdminDashboard";
-import AuthCallback from "../pages/AuthCallback"; // 새로 추가
+import AuthCallback from "../pages/AuthCallback";
+import ReportIncident from "../pages/ReportIncident/ReportIncident";
+import ViewReports from "../pages/ViewReports/ViewReports";
+import ManageReports from "../pages/ManageReports/ManageReports";
+import Chatbot from "../components/chatbot/Chatbot"; // Chatbot 페이지 추가
 
 function AppRoutes() {
   return (
@@ -13,6 +17,14 @@ function AppRoutes() {
       <Route path="/user-dashboard" element={<UserDashboard />} />
       <Route path="/admin-dashboard" element={<AdminDashboard />} />
       <Route path="/auth/kakao/callback" element={<AuthCallback />} />
+
+      {/* 신고 및 관리 페이지 경로 설정 */}
+      <Route path="/report-incident" element={<ReportIncident />} />
+      <Route path="/view-reports" element={<ViewReports />} />
+      <Route path="/manage-reports" element={<ManageReports />} />
+      
+      {/* Chatbot 경로 추가 */}
+      <Route path="/chatbot" element={<Chatbot />} />
     </Routes>
   );
 }
